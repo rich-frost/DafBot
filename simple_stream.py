81bbb49368c19b5b63e75b6baef5242f395c006d
ben-green-kerro <ben@kernowrobotics.co.uk>
2022-06-21 20:49:00 +0100

    changed demo routine

--- simple_stream.py
+++ simple_stream.py
@@ -15,18 +15,15 @@ for p in port:
 print('Connecting: ' + serstring)
 s = serial.Serial(serstring, 115200)
 
-# Open g-code file
-f = open('demo.gcode', 'r')
-
 # Wake up grbl
 s.write("\r\n\r\n".encode())
 print('Warming up')
 time.sleep(2)   # Wait for grbl to initialize
 s.flushInput()  # Flush startup text in serial input
 
-coords =   [[200, 0],
-            [200, 200],
-            [0, 200],
+coords =   [[380, 0],
+            [380, 400],
+            [0, 400],
             [0, 0],
             [50, 0],
             [50, 50],
@@ -70,18 +67,4 @@ while(True):
         move(coord)
     time.sleep(2)
 
-
-# Stream g-code to grbl
-# for line in f:
-#     l = line.strip()  # Strip all EOL characters for consistency
-#     print('Sending: ' + l)
-#     s.write((l + '\n').encode())  # Send g-code block to grbl
-#     grbl_out = s.readline()  # Wait for grbl response with carriage return
-#     print(' : ' + grbl_out.strip().decode())
-
-# Wait here until grbl is finished to close serial port and file.
-# input("  Press <Enter> to exit and disable grbl.")
-
-# Close file and serial port
-# f.close()
 s.close()
