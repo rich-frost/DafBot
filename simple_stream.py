4383cf7856bc90e6de1137a0e97b8b371c862f41
ben-green-kerro <ben@kernowrobotics.co.uk>
2022-06-21 13:29:25 +0100

    changed demo routine

--- simple_stream.py
+++ simple_stream.py
@@ -1,51 +1,12 @@
-#!/usr/bin/env python
-"""\
-Simple g-code streaming script for grbl
-
-Provided as an illustration of the basic communication interface
-for grbl. When grbl has finished parsing the g-code block, it will
-return an 'ok' or 'error' response. When the planner buffer is full,
-grbl will not send a response until the planner buffer clears space.
-
-G02/03 arcs are special exceptions, where they inject short line
-segments directly into the planner. So there may not be a response
-from grbl for the duration of the arc.
-
----------------------
-The MIT License (MIT)
-
-Copyright (c) 2012 Sungeun K. Jeon
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
----------------------
-"""
-
+#!/usr/bin/env python3
 import time
 
 import serial
-# Open grbl serial port
 from serial.tools import list_ports
 
 port = list(list_ports.comports())
 serstring = ''
 for p in port:
-
     if '/dev/cu.usbmodem' in p.device:
         serstring = p.device
         break
@@ -59,20 +20,68 @@ f = open('demo.gcode', 'r')
 
 # Wake up grbl
 s.write("\r\n\r\n".encode())
+print('Warming up')
 time.sleep(2)   # Wait for grbl to initialize
 s.flushInput()  # Flush startup text in serial input
 
-# Stream g-code to grbl
-for line in f:
-    l = line.strip()  # Strip all EOL characters for consistency
-    print('Sending: ' + l,)
+coords =   [[200, 0],
+            [200, 200],
+            [0, 200],
+            [0, 0],
+            [50, 0],
+            [50, 50],
+            [0, 50],
+            [0, 0]]
+x = 0
+y = 0
+f = 10000
+
+
+def move(coord):
+    x = coord[0]
+    y = coord[1]
+    sleep=0.01
+    l = 'G1 X{} Y{} F{}'.format(x, y, f)
+    print('Sending: ' + l)
     s.write((l + '\n').encode())  # Send g-code block to grbl
-    grbl_out = s.readline()  # Wait for grbl response with carriage return
-    print(' : ' + grbl_out.strip().decode())
+    time.sleep(sleep)
+    # Wait for grbl response with carriage return
+    grbl_out = s.readline().strip().decode()
+    if grbl_out == 'ok':
+        print('moving')
+    else:
+        print(' : ' + grbl_out)
+    time.sleep(sleep)
+    l = 'G4 P0'
+    s.write((l + '\n').encode())  # Send g-code block to grbl
+    time.sleep(sleep)
+    # Wait for grbl response with carriage return
+    grbl_out = s.readline().strip().decode()
+    if grbl_out == 'ok':
+        print('Done :)')
+    else:
+        print('ERROR!')
+        print(' : ' + grbl_out)
+    time.sleep(sleep)
+
+
+while(True):
+    for coord in coords:
+        move(coord)
+    time.sleep(2)
+
+
+# Stream g-code to grbl
+# for line in f:
+#     l = line.strip()  # Strip all EOL characters for consistency
+#     print('Sending: ' + l)
+#     s.write((l + '\n').encode())  # Send g-code block to grbl
+#     grbl_out = s.readline()  # Wait for grbl response with carriage return
+#     print(' : ' + grbl_out.strip().decode())
 
 # Wait here until grbl is finished to close serial port and file.
-input("  Press <Enter> to exit and disable grbl.")
+# input("  Press <Enter> to exit and disable grbl.")
 
 # Close file and serial port
-f.close()
+# f.close()
 s.close()
