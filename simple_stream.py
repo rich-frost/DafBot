363b5272ec5396d2f92b7a82f9dd4d5301f69704
ben-green-kerro <ben@kernowrobotics.co.uk>
2022-06-23 08:53:24 +0100

    Added homing handler on strl-c

--- simple_stream.py
+++ simple_stream.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import random
+import signal
 import time
 
 import serial
@@ -12,7 +14,7 @@ for p in port:
         break
     else:
         print('Skipping: ' + p.device)
-if serstring =='':
+if serstring == '':
     print('No valid serial devices found')
     print('EXITING')
     quit()
@@ -25,32 +27,44 @@ s.write("\r\n\r\n".encode())
 print('Warming up')
 time.sleep(2)   # Wait for grbl to initialize
 s.flushInput()  # Flush startup text in serial input
-
-coords =   [[380, 0],
-            [380, 400],
-            [0, 400],
-            [0, 0],
-            [50, 0],
-            [50, 50],
-            [0, 50],
-            [0, 0]]
+limits = [[0, 385], [0, 400]]
+coords = [[385, 0],
+          [385, 400],
+          [0, 400],
+          [0, 0],
+          [50, 0],
+          [50, 50],
+          [0, 50],
+          [0, 0]]
 x = 0
 y = 0
-f = 10000
+
+exiting = 0
 
 
-def move(coord):
+def handler(signum, frame):
+    global exiting
+    print('')
+    # print('exiting now')
+    exiting = 1
+
+
+signal.signal(signal.SIGINT, handler)
+
+
+def move(coord, speed):
     x = coord[0]
     y = coord[1]
-    sleep=0.01
-    l = 'G1 X{} Y{} F{}'.format(x, y, f)
+    sleep = 0.01
+    l = 'G1 X{} Y{} F{}'.format(x, y, speed)
     print('Sending: ' + l)
     s.write((l + '\n').encode())  # Send g-code block to grbl
     time.sleep(sleep)
     # Wait for grbl response with carriage return
     grbl_out = s.readline().strip().decode()
     if grbl_out == 'ok':
-        print('moving')
+        # print('moving')
+        pass
     else:
         print(' : ' + grbl_out)
     time.sleep(sleep)
@@ -60,16 +74,21 @@ def move(coord):
     # Wait for grbl response with carriage return
     grbl_out = s.readline().strip().decode()
     if grbl_out == 'ok':
-        print('Done :)')
+        # print('Done :)')
+        pass
     else:
         print('ERROR!')
         print(' : ' + grbl_out)
     time.sleep(sleep)
 
 
-while(True):
-    for coord in coords:
-        move(coord)
-    time.sleep(2)
+while(exiting == 0):
+    # for coord in coords:
+    #     move(coord, 10000)
 
+    coord = [random.randint(0, limits[0][1]), random.randint(0, limits[1][1])]
+    move(coord, 10000)
+    # time.sleep(2)
+print('homing before quiting!')
+move([0, 0], 5000)
 s.close()
