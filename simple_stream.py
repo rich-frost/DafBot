ad961d5eb2135feb955d95f94b46474b7706985e
ben-green-kerro <ben@kernowrobotics.co.uk>
2022-06-25 22:43:18 +0100

    de-linting

--- simple_stream.py
+++ simple_stream.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
+"""
+Demo gantry gcode driver code for DafBot. Can move through random positions
+within the bounds or do standard test procedure.
+"""
 import random
 import signal
+import sys
 import time
 
 import serial
 from serial.tools import list_ports
 
 port = list(list_ports.comports())
-serstring = ''
+SETSTRING = ''
 for p in port:
-    if '/dev/cu.usbmodem' in p.device:
-        serstring = p.device
-        break
-    else:
+    if '/dev/cu.usbmodem' not in p.device:
         print('Skipping: ' + p.device)
-if serstring == '':
+    else:
+        SERIAL_STRING = p.device
+        break
+if SERIAL_STRING == '':
     print('No valid serial devices found')
     print('EXITING')
-    quit()
+    sys.exit()
 else:
-    print('Connecting: ' + serstring)
-    s = serial.Serial(serstring, 115200)
+    print('Connecting: ' + SERIAL_STRING)
+    s = serial.Serial(SERIAL_STRING, 115200)
 s.write("\r\n\r\n".encode())
 print('Warming up')
 time.sleep(2)
@@ -34,30 +39,32 @@ coords = [[385, 0],
           [50, 50],
           [0, 50],
           [0, 0]]
-x = 0
-y = 0
-exiting = 0
-stepspermm = (200 * 16) / (20 * 2)
+EXITING = 0
+STEPS_PER_MM = (200 * 16) / (20 * 2)
 
 
-def handler(signum, frame):
-    global exiting
-    print('')
+def handler():
+    """
+    This is the handler for SIGINT events sent by the user pressing ctrl-c.
+    """
+    global EXITING
+    # print('')
     # print('exiting now')
-    exiting = 1
+    EXITING = 1
 
 
 signal.signal(signal.SIGINT, handler)
 
 
-def move(coord, speed):
-    global exiting
-    x = coord[0]
-    y = coord[1]
+def move(target_coordinate, speed):
+    """
+    This function is responsible for driving the gantry to the desired position
+    and stalling the code function until that position is reached.
+    """
     sleep = 0.01
-    l = 'G1 X{:<3} Y{:<3} F{}'.format(x, y, speed)
-    print('sending: ' + l)
-    s.write((l + '\n').encode())  # Send g-code block to grbl
+    next_gcode_line = f'G1 X{target_coordinate[0]} Y{target_coordinate[1]} F{speed}'
+    print('sending: ' + next_gcode_line)
+    s.write((next_gcode_line + '\n').encode())  # Send g-code block to grbl
     time.sleep(sleep)
     # Wait for grbl response with carriage return
     grbl_out = s.readline().strip().decode()
@@ -67,8 +74,8 @@ def move(coord, speed):
     else:
         print(' : ' + grbl_out)
     time.sleep(sleep)
-    l = 'G4 P0'
-    s.write((l + '\n').encode())  # Send g-code block to grbl
+    next_gcode_line = 'G4 P0'
+    s.write((next_gcode_line + '\n').encode())  # Send g-code block to grbl
     time.sleep(sleep)
     # Wait for grbl response with carriage return
     grbl_out = s.readline().strip().decode()
@@ -83,7 +90,7 @@ def move(coord, speed):
     time.sleep(sleep)
 
 
-while(exiting == 0):
+while EXITING == 0:
     # for coord in coords:
     #     move(coord, 10000)
     coord = [random.randint(0, limits[0][1]), random.randint(0, limits[1][1])]
