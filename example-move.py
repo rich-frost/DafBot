d237ee6bfe2987047f6c3fbe10991b5ea18e169c
Ben Green <ben@kernowrobotics.co.uk>
2022-11-05 16:29:29 +0000

    example movements modified

--- example-move.py
+++ example-move.py
@@ -1,8 +1,15 @@
 from gantry import Gantry
 
 dafBot = Gantry('/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_75735303931351A071E2-if00')
-dafBot.homex()
+dafBot.send_command('$HZ', 1)
+# dafBot.move_new([10,10,10], 8000)
+
+# dafBot.send_command('G28 X', 1)
+# dafBot.send_command('$HX', 1)
+
+# dafBot.homex()
+# dafBot.move_new([10,10,0],11000)
 # while True:
-#     dafBot.move([100,100,100,8000])
-#     dafBot.move([0,0,0,8000])
+#     dafBot.move_new([10,10,10],11000)
+    # dafBot.move_new([0,0,0],11000)
     
