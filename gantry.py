9a07dbd66f34a96d645ccb97b6d5ea49a165c1d4
Ben Green <ben@kernowrobotics.co.uk>
2022-11-05 16:29:10 +0000

    refactored class methods

--- gantry.py
+++ gantry.py
@@ -35,6 +35,12 @@ class Gantry(object):
         else:
             print('Connecting: ' + self.serial_string)
             self.s = serial.Serial(self.serial_string, 115200)
+            self.s.write("\r\n\r\n".encode())
+            print('Warming up - grbl')
+            time.sleep(2)
+            self.s.flushInput()
+        self.send_command('$22=1')
+        self.send_command('$23=3')
 
 
     def handler(self, signum, frame):  # pylint: disable=W0613
@@ -46,6 +52,15 @@ class Gantry(object):
         self.exiting = 1
         sys.exit()
 
+    def move_new(self, target_coordinate, speed):
+        self.s.flushInput()
+        time.sleep(0.01)
+        next_gcode_line = f'G1 X{target_coordinate[0]} Y{target_coordinate[1]} Z{target_coordinate[2]} F{speed}'
+        time.sleep(0.01)
+        self.send_command(next_gcode_line, 1)
+        time.sleep(0.01)
+        self.wait_until_finished()
+        time.sleep(0.01)
 
 
     def move(self, target_coordinate, speed):
@@ -84,13 +99,36 @@ class Gantry(object):
         time.sleep(sleep)
 
 
+    def send_command(self, command = '', verbose=''):
+        self.s.flushInput()
+        time.sleep(0.01)
+        if verbose == 1:
+            print('sending: ' + command)
+        self.s.write((command + '\n').encode())
+        time.sleep(0.01)
+        grbl_out = self.s.readline().strip().decode()
+        # print(f'message recieved = {grbl_out}')
+        if verbose == 1:
+            print(grbl_out)
+        # if grbl_out == 'ok':
+        #     print('moving')
+        #     pass
+        # else:
+        #     print(' : ' + grbl_out)
+        time.sleep(0.01)
+
+
+    def wait_until_finished(self):
+        print('waiting for completion')
+        self.send_command('G4 P0')
+
     def homex(self):
         """
         This function is responsible for driving the gantry to the desired position
         and stalling the code function until that position is reached.
         """
         sleep = 0.01
-        next_gcode_line = f'G28 X'
+        next_gcode_line = 'G28 X0'
         print('sending: ' + next_gcode_line)
         self.s.write((next_gcode_line + '\n').encode())  # Send g-code block to grbl
         time.sleep(sleep)
@@ -103,6 +141,8 @@ class Gantry(object):
         else:
             print(' : ' + grbl_out)
         time.sleep(sleep)
+
+
         next_gcode_line = 'G4 P0'
         self.s.write((next_gcode_line + '\n').encode())  # Send g-code block to grbl
         time.sleep(sleep)
